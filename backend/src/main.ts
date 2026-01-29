97c82e36d85bc83e5481a1ce0acf4fe2619f6cc1
Juan-Camilo-Sanchez-Echeverri <juancamilosanche65@gmail.com>
2026-01-29 18:59:53 -0500

    feat: add compression and helmet middleware for security and performance

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,52 @@
 import { NestFactory } from '@nestjs/core';
+
+import { ConsoleLogger, VersioningType } from '@nestjs/common';
+
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import compression from 'compression';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+const logger = new ConsoleLogger({ prefix: 'Analytics Backend' });
+
+async function bootstrap(): Promise<void> {
+  /**
+   * Create the application.
+   */
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger,
+  });
+
+  /**
+   * Use helmet and compression for security and performance.
+   */
+  app.use(compression());
+  app.use(helmet());
+
+  /**
+   * Set various application settings.
+   */
+  app.set('trust proxy', true);
+  app.set('query parser', 'extended');
+
+  /**
+   * Set the global prefix.
+   */
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+    defaultVersion: '1.0',
+  });
+
+  await app.listen(3000);
+  logger.log(`Server running on ${3000}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  logger.error('Error during app bootstrap', err);
+  process.exit(1);
+});
